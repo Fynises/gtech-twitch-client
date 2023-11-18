33592158154296a64c6ff4ae9404d7f5f73bac66
Fynises <fynises.dev@proton.me>
2023-11-18 21:19:41 +0000

    fix barrel optimization warnings

--- src/components/dashboard/styled-button.tsx
+++ src/components/dashboard/styled-button.tsx
@@ -1,5 +1,6 @@
 import { THEME_YELLOW } from '@/util/colors';
-import { Button, styled } from '@mui/material';
+import { Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 export const CustomButton = styled(Button)({
   backgroundColor: THEME_YELLOW,
